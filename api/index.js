9138ff70acca193599abed568a99ee1af1ecc937
razod <masonboulier@gmail.com>
2022-05-08 15:29:50 -0400

    API: creating users + authenticating users

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const cfg = require('./config');
+const mongoose = require('mongoose');
+const cfg = require('./config.json');
 
+// Routes
+const auth = require('./routes/api/auth');
+const users = require('./routes/api/users');
 
 const app = express();
 app.use(express.json());
+
+mongoose
+    .connect(cfg.mongoDB)
+    .then(() => console.log('Nexhlabs-API Connected to database!'))
+    .catch(err => console.log(err));
+
+app.use('/api/auth', auth); 
+app.use('/api/users', users);
+
 
 app.listen(cfg.port, () => console.log(`Nexhlabs-API running on http://localhost:${cfg.port}`));

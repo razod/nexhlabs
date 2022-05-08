dda9a61a5148566ae7cd220a4328bcc5ce722ffc
razod <masonboulier@gmail.com>
2022-05-08 15:40:35 -0400

    add css to client

--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+var path = require('path');
 
 var app = express();
+app.use('/public', express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index.hbs');
+})
 
 app.listen(1111, () => console.log("Nexhlabs-Client running on http://localhost:1111"));

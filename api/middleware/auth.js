e9f2943f4e122f7cb7f9a11a9dc46c1ead56b5be
razod <masonboulier@gmail.com>
2022-05-08 18:13:59 -0400

    res.status("401") -> res.status(401)

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -5,7 +5,7 @@ function auth(req, res, next) {
 
     const token = req.header('x-auth-token');
 
-    if(!token) return res.status('401').json({ msg: 'No Token, authorization denied'});
+    if(!token) return res.status(401).json({ msg: 'No Token, authorization denied'});
     
     try {
         const decoded = jwt.verify(token, config.secret);

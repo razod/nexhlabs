f48e4e04a8cf9de0a8e8a7155f60cffabfcd7b06
razod <masonboulier@gmail.com>
2022-05-08 15:03:37 -0400

    ez

--- client/index.js
+++ client/index.js
@@ -2,4 +2,4 @@ const express = require('express');
 
 var app = express();
 
-app.listen(1111, () => console.log("Nexhlabs Client running on http://localhost:1111"));
+app.listen(1111, () => console.log("Nexhlabs-Client running on http://localhost:1111"));
---
+++ nexhlabs.js
@@ -0,0 +1,2 @@
+require("./client/index");
+require("./api/index")
